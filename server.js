2e28be1971269ef8703e9f4c7064f2a41433fe81
Hammad-Shah804 <newslowermail@gmail.com>
2026-05-09 17:09:49 +0500

    initial commit

--- server.js
+++ server.js
@@ -3,79 +3,54 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
 
-const io = new Server(server,{
-  cors:{
-    origin:"*"
-  }
+const io = new Server(server, {
+  cors: { origin: "*" }
 });
 
 /* Pages */
-
-app.get("/admin",(req,res)=>{
+app.get("/admin", (req, res) => {
   res.sendFile(__dirname + "/admin.html");
 });
 
-app.get("/user",(req,res)=>{
+app.get("/user", (req, res) => {
   res.sendFile(__dirname + "/user.html");
 });
 
-/* Socket */
+/* Socket Logic */
+io.on("connection", (socket) => {
 
-io.on("connection",(socket)=>{
+  console.log("User connected");
 
-  console.log("User Connected");
-
-  /* Admin Join */
-
-  socket.on("admin-join",()=>{
-
+  // Admin join
+  socket.on("admin-join", () => {
     socket.join("admin-room");
-
-    console.log("Admin Joined");
-
   });
 
-  /* User Join */
-
-  socket.on("user-join",()=>{
-
+  // User join
+  socket.on("user-join", () => {
     socket.join("user-room");
-
-    console.log("User Joined");
-
   });
 
-  /* Admin Message */
-
-  socket.on("admin-message",(msg)=>{
-
-    io.to("user-room").emit("receive-message",{
-      sender:"Admin",
-      text:msg
+  // Admin message → User
+  socket.on("admin-message", (msg) => {
+    io.to("user-room").emit("message", {
+      sender: "Admin",
+      text: msg
     });
-
   });
 
-  /* User Message */
-
-  socket.on("user-message",(msg)=>{
-
-    io.to("admin-room").emit("receive-message",{
-      sender:"User",
-      text:msg
+  // User message → Admin
+  socket.on("user-message", (msg) => {
+    io.to("admin-room").emit("message", {
+      sender: "User",
+      text: msg
     });
-
   });
 
 });
 
-/* Server */
-
-server.listen(8080,"0.0.0.0",()=>{
-
-  console.log("Server Running On 8080");
-
+server.listen(8080, "0.0.0.0", () => {
+  console.log("Server running on port 8080");
 });
