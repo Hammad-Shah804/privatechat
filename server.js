c26b5ba5595d258b70fb3eaef05a392634fcacde
Hammad-Shah804 <newslowermail@gmail.com>
2026-05-09 15:59:02 +0500

    private chat upload

--- server.js
+++ server.js
@@ -6,28 +6,76 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "*"
+const io = new Server(server,{
+  cors:{
+    origin:"*"
   }
 });
 
-app.use(express.static(__dirname));
+/* Pages */
 
-io.on("connection", (socket) => {
+app.get("/admin",(req,res)=>{
+  res.sendFile(__dirname + "/admin.html");
+});
+
+app.get("/user",(req,res)=>{
+  res.sendFile(__dirname + "/user.html");
+});
+
+/* Socket */
+
+io.on("connection",(socket)=>{
 
   console.log("User Connected");
 
-  socket.on("message", (msg) => {
+  /* Admin Join */
 
-    socket.broadcast.emit("message", msg);
+  socket.on("admin-join",()=>{
+
+    socket.join("admin-room");
+
+    console.log("Admin Joined");
+
+  });
+
+  /* User Join */
+
+  socket.on("user-join",()=>{
+
+    socket.join("user-room");
+
+    console.log("User Joined");
+
+  });
+
+  /* Admin Message */
+
+  socket.on("admin-message",(msg)=>{
+
+    io.to("user-room").emit("receive-message",{
+      sender:"Admin",
+      text:msg
+    });
+
+  });
+
+  /* User Message */
+
+  socket.on("user-message",(msg)=>{
+
+    io.to("admin-room").emit("receive-message",{
+      sender:"User",
+      text:msg
+    });
 
   });
 
 });
 
-server.listen(3000, () => {
+/* Server */
 
-  console.log("Server Running");
+server.listen(8080,"0.0.0.0",()=>{
+
+  console.log("Server Running On 8080");
 
 });
